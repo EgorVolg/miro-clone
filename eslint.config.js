fa91bfba81fcf751e784116d54e03d7ee8d1989b
Egor Volg <duna9588@gmail.com>
2025-06-02 09:42:46 +0300

    feat: add Prettier configuration and update ESLint settings

--- eslint.config.js
+++ eslint.config.js
@@ -8,7 +8,11 @@ import { eslintBoundariesConfig } from "./eslint.boundaries.js";
 export default tseslint.config(
   { ignores: ["dist"] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      prettier.recommended,
+    ],
     files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
