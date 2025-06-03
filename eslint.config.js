4edc7dc25b3637a2a0b40a5ff5b18435c34f00e7
Egor Volg <duna9588@gmail.com>
2025-06-03 11:28:00 +0300

    feat: add environment configuration and update ESLint settings

--- eslint.config.js
+++ eslint.config.js
@@ -8,11 +8,7 @@ import { eslintBoundariesConfig } from "./eslint.boundaries.js";
 export default tseslint.config(
   { ignores: ["dist"] },
   {
-    extends: [
-      js.configs.recommended,
-      ...tseslint.configs.recommended,
-      prettier.recommended,
-    ],
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
